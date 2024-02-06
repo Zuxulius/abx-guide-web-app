a9facc8fedd6d114251f45d27bccf976137096f5
Axel <70969800+Zuxulius@users.noreply.github.com>
2024-02-06 17:28:17 +0100

    Create header for homepage

--- abx-guide-web-app/src/Header.jsx
+++ abx-guide-web-app/src/Header.jsx
@@ -1,8 +1,15 @@
+import logo from "./assets/abx-guide-logo.png"
 
 function Header() {
 
     return (
-        <></>
+        <header>
+            <img src={logo} alt="Logo, Region Østergötland"
+            height={75}
+            width={300}
+            />
+            <h1>ANTIBIOTIKA PÅ AKUTEN</h1>
+        </header>
     )
 }
 
--- abx-guide-web-app/src/Home.jsx
+++ abx-guide-web-app/src/Home.jsx
@@ -1,8 +1,9 @@
+import Header from "./Header";
 
 function Home() {
 
     return(
-        <></>
+        <Header />
     )
 }
 
